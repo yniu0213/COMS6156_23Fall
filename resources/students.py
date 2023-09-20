96a0ad68d9c170af0b46e70fb67ca3eb72c198fd
yniu0213 <yn2433@columbia.edu>
2023-09-19 20:58:11 -0400

    for hw1

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "C:/Users/76784/Desktop/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
